691c59bf5045c2bd0dd55278f29804279858067e
trtl_playz <trtlplayzyt@gmail.com>
2026-09-12 14:29:02 -0700

    change inputs

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,16 @@
 extends CharacterBody2D
 
-const JUMP_VELOCITY = -900.0
+const jumpVel = -900.0
+const antiJumpVel = 900.0
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	if Input.is_action_just_pressed("ui_accept"):
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = jumpVel
+	
+	if Input.is_action_just_pressed("anti-jump"):
+		velocity.y = antiJumpVel
 	
 	move_and_slide()
